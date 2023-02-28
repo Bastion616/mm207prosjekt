add823a69c7f7724ab6814947ddc058a6b95b150
Bastion616 <albert.sudmann@outlook.com>
2023-02-28 10:14:51 +0100

    test

--- server.mjs
+++ server.mjs
@@ -1,5 +1,5 @@
 import express from 'express'
-import Dictionary from './DictionaryModule/dictionary.mjs';
+import Dictionary from './public/DictionaryModule/dictionary.mjs';
 
 const server = express();
 const port = (process.env.PORT || 8080);
