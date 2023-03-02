c5d479599b64b6ae87c34e793bc3cd4803413ae6
Bastion616 <albert.sudmann@outlook.com>
2023-03-02 16:37:35 +0100

    Wishlist finished

--- public/DictionaryModule/dictionary.mjs
+++ public/DictionaryModule/dictionary.mjs
@@ -1,17 +1,19 @@
 const Dictionary = {
     en:{
-        "h2": "This application shows how a Pokédex works, but to save resources it only shows the first generation of Pokémon. There are in total over 1000 different Pokémon but including all of them increass the loading time. Choose a Pokémon and click Add to wishlist to add it to your wishlist. You can remove it there if you regret it",
+        "h2": "This application shows how a Pokédex works, but to save resources it only shows the first generation of Pokémon. Choose a Pokémon and click Add to wishlist to add it to your wishlist. You can remove it there if you regret it",
         "normal" : "Normal",
         "shiny" : "Shiny",
         "wishlist" : "Wishlist",
-        "addToWishlist" : "Add to wishlist"
+        "addToWishlist" : "Add to wishlist",
+        "deleteFromWishlist" : "Delete"
     },
     no:{
-        "h2": "Denne applikasjonen viser hvordan en Pokédex fungerer, men for å spare på resurser viser den bare den første generasjonen med Pokémon. Det finnes over 1000 men da tar det veldig lang tid å laste inn. Velg en Pokémon og trykk på Legg til i ønskeliste for å legge den til i ønskelisten! Der kan du fjerne den igjen om du skulle angre",
+        "h2": "Denne applikasjonen viser hvordan en Pokédex fungerer, men for å spare på resurser viser den bare den første generasjonen med Pokémon. Velg en Pokémon og trykk på Legg til i ønskeliste for å legge den til i ønskelisten! Der kan du fjerne den igjen om du skulle angre",
         "normal" : "Vanlig",
         "shiny" : "Skinnende",
         "wishlist" : "Ønskeliste",
-        "addToWishlist" : "Legg til i ønskeliste"
+        "addToWishlist" : "Legg til i ønskeliste",
+        "deleteFromWishlist" : "slett"
     }
 }   
 
--- public/index.mjs
+++ public/index.mjs
@@ -1,78 +1,68 @@
 import Dictionary from './DictionaryModule/dictionary.mjs';
-console.log(Dictionary);
 
 const pokemonCount = 151;
-let pokedex = {}; 
-let noBtn = document.getElementById("no");
-let enBtn = document.getElementById("en");
-let normalBtn = document.getElementById("normal");
-let shinyBtn = document.getElementById("shiny");
-let wishlistBtn = document.getElementById("wishlist");
-let addToWishlistBtn = document.getElementById("addToWishlist");
+let pokedex = {};
+let selectedPokemonId = null; // variable to keep track of the selected Pokemon id
+const noBtn = document.getElementById("no");
+const enBtn = document.getElementById("en");
+const normalBtn = document.getElementById("normal");
+const shinyBtn = document.getElementById("shiny");
 
-window.onload = async function() 
-{
+const addToWishlistBtn = document.getElementById("addToWishlist");
 
-    for (let i = 1; i <= pokemonCount; i++)
-    {
+window.onload = async function () {
+
+    for (let i = 1; i <= pokemonCount; i++) {
         await getPokemon(i);
 
         //Gets the names and id of the pokemon and displays them in the div in uppercase. 
         let pokemon = document.createElement("div");
         pokemon.id = i;
-        pokemon.innerText = i.toString() + ". " + pokedex[i]["name"].toUpperCase(); 
-        pokemon.classList.add("pokemon-name");
+        pokemon.innerText = i.toString() + ". " + pokedex[i]["name"].toUpperCase();
+        pokemon.classList.add("pokemonName");
 
-        
 
-        pokemon.addEventListener("click", function updatePokemon()
-        {
-            document.getElementById("pokemon-img").src = pokedex[this.id]["img"];
+        pokemon.addEventListener("click", function updatePokemon() {
+            document.getElementById("pokemonImg").src = pokedex[this.id]["img"];
 
             //Remove previous type
-            let typesDiv = document.getElementById("pokemon-types");
-            while (typesDiv.firstChild) 
-            {
+            let typesDiv = document.getElementById("pokemonTypes");
+            while (typesDiv.firstChild) {
                 typesDiv.firstChild.remove();
             }
 
             //Update types
             let types = pokedex[this.id]["types"];
-            for (let i = 0; i < types.length; i++) 
-            {
+            for (let i = 0; i < types.length; i++) {
                 let type = document.createElement("span");
                 type.innerText = types[i]["type"]["name"].toUpperCase();
-                type.classList.add("type-box");
+                type.classList.add("typeBox");
                 type.classList.add(types[i]["type"]["name"]);
-                typesDiv.append(type); 
+                typesDiv.append(type);
             }
 
             //Update descripton
-            document.getElementById("pokemon-description").innerText = pokedex[this.id]["desc"];
+            document.getElementById("pokemonDescription").innerText = pokedex[this.id]["desc"];
 
-             //Update image
+            //Update image
             let pokemonImg = pokedex[this.id]["img"];
             let pokemonShiny = pokedex[this.id]["shinyImg"];
-            if (document.getElementById("normal").checked) 
-            {
-                document.getElementById("pokemon-img").src = pokemonImg;
-            } 
-            else if (document.getElementById("shiny").checked) 
-            {
-                document.getElementById("pokemon-img").src = pokemonShiny;
+            if (document.getElementById("normal").checked) {
+                document.getElementById("pokemonImg").src = pokemonImg;
+            }
+            else if (document.getElementById("shiny").checked) {
+                document.getElementById("pokemonImg").src = pokemonShiny;
             }
         });
 
-        document.getElementById("pokemon-description").innerText = pokedex[1]["desc"];
+        document.getElementById("pokemonDescription").innerText = pokedex[1]["desc"];
 
-        document.getElementById("pokemon-list").append(pokemon);
+        document.getElementById("pokemonList").append(pokemon);
     }
-
     console.log(pokedex);
 }
 
-async function getPokemon(num) 
-{
+async function getPokemon(num) {
     let url = "https://pokeapi.co/api/v2/pokemon/" + num.toString();
 
     let res = await fetch(url);
@@ -83,37 +73,74 @@ async function getPokemon(num)
     let pokemonType = pokemon["types"];
     let pokemonImg = pokemon["sprites"]["front_default"];
     let pokemonShiny = pokemon["sprites"]["front_shiny"];
-    
+
 
     res = await fetch(pokemon["species"]["url"]);
     let pokemonDesc = await res.json();
     pokemonDesc = pokemonDesc["flavor_text_entries"][9]["flavor_text"];
 
-    pokedex[num] = {"name" : pokemonName, "img" : pokemonImg, "types" : pokemonType, "desc" : pokemonDesc};
+    pokedex[num] = { "name": pokemonName, "img": pokemonImg, "types": pokemonType, "desc": pokemonDesc };
 
-    normalBtn.addEventListener("click", function normalImg()
-    {
-        pokedex[num] = {"name" : pokemonName, "img" : pokemonImg, "types" : pokemonType, "desc" : pokemonDesc};
+    normalBtn.addEventListener("click", function normalImg() {
+        pokedex[num] = { "name": pokemonName, "img": pokemonImg, "types": pokemonType, "desc": pokemonDesc };
     });
 
-    shinyBtn.addEventListener("click", function shinyImg()
-    {
-        pokedex[num] = {"name" : pokemonName, "img" : pokemonShiny, "types" : pokemonType, "desc" : pokemonDesc};
+    shinyBtn.addEventListener("click", function shinyImg() {
+        pokedex[num] = { "name": pokemonName, "img": pokemonShiny, "types": pokemonType, "desc": pokemonDesc };
     });
 }
 
-noBtn.addEventListener("click", async function(evt){
+noBtn.addEventListener("click", async function (evt) {
     h2.innerHTML = Dictionary.no.h2;
     normalBtn.innerHTML = Dictionary.no.normal;
     shinyBtn.innerHTML = Dictionary.no.shiny;
-    wishlistBtn.innerHTML = Dictionary.no.wishlist;
     addToWishlistBtn.innerHTML = Dictionary.no.addToWishlist;
 });
 
-enBtn.addEventListener("click", async function(evt){
+enBtn.addEventListener("click", async function (evt) {
     h2.innerHTML = Dictionary.en.h2;
     normalBtn.innerHTML = Dictionary.en.normal;
     shinyBtn.innerHTML = Dictionary.en.shiny;
-    wishlistBtn.innerHTML = Dictionary.en.wishlist;
     addToWishlistBtn.innerHTML = Dictionary.en.addToWishlist;
 });
+
+
+let numOfItems = 0;
+let wishlistContent = document.getElementById("wishlistContent");
+
+addToWishlistBtn.addEventListener("click", function () {
+
+    for (let i = 1; i <= pokemonCount; i++) {
+        let pokemon = document.getElementById(i);
+        pokemon.addEventListener("click", function () {
+            selectedPokemonId = i; // update the selected Pokemon id
+        });
+    }
+
+    if (selectedPokemonId) {
+        numOfItems++;
+        let wishlistItem = document.createElement("div");
+        let wishlistItemImage = document.createElement("img");
+        let wishlistItemName = document.createElement("p");
+        wishlistItemImage.src = pokedex[selectedPokemonId]["img"];
+        wishlistItemName.innerHTML = pokedex[selectedPokemonId]["name"].toUpperCase();
+        let wishlistDelete = document.createElement("button");
+        wishlistDelete.innerHTML = "Delete";
+
+        wishlistContent.appendChild(wishlistItem);
+        wishlistItem.appendChild(wishlistItemImage);
+        wishlistItem.appendChild(wishlistItemName);
+        wishlistItem.appendChild(wishlistDelete);
+
+        console.log(numOfItems);
+
+        wishlistDelete.addEventListener("click", function () {
+            wishlistItem.remove(); // remove the wishlist item from the wishlistContent div
+            numOfItems--; // decrease the number of items in the wishlist
+            console.log(numOfItems);
+        });
+    }
+});
+
+
+
